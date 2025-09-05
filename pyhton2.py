d503482fe9bb96b1579585ac1104b024669131cf
MOHD AFFAN <mohdaffan.hacker@gmail.com>
2025-09-05 15:37:57 +0530

    practicing pull request as friend changed in python2.py

--- pyhton2.py
+++ pyhton2.py
@@ -2,3 +2,4 @@ a=5
 b=6
 print(a*b)
 print("undo commit practice")
+print("now i am  practicing pull request as one of my friend changed my work style in his computer now i also have to add in my local system")
